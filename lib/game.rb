13d0d2f780a74d9e0d9c6bdd6a69645a6fafe918
Colin Kennedy <colin.kennedy@intercom.io>
2018-11-07 16:53:09 +0000

    A solution to the kata

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,49 @@
 class Game
+  attr_accessor :score, :rolls
 
+  def initialize
+    @score = 0
+    @rolls = []
+  end
+
+  def roll(pins)
+    rolls.push(pins)
+  end
+
+  def score
+    frame_index = 0
+    10.times do
+      if is_strike?(frame_index)
+        @score += 10 + strike_bonus(frame_index)
+        frame_index += 1
+      elsif is_spare?(frame_index)
+        @score += 10 + spare_bonus(frame_index)
+        frame_index += 2
+      else
+        @score += frame_score(frame_index)
+        frame_index += 2
+      end
+    end
+    @score
+  end
+
+  private def is_strike?(frame_index)
+    rolls[frame_index] == 10
+  end
+
+  private def is_spare?(frame_index)
+    frame_score(frame_index) == 10
+  end
+
+  private def frame_score(frame_index)
+    rolls[frame_index] + rolls[frame_index + 1]
+  end
+
+  private def strike_bonus(frame_index)
+    rolls[frame_index + 1] + rolls[frame_index + 2]
+  end
+
+  private def spare_bonus(frame_index)
+    rolls[frame_index + 2]
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,66 @@
 require "game"
 
+def roll_many(pins, times)
+  times.times do
+    game.roll(pins)
+  end
+end
+
+def roll_spare
+  game.roll(5)
+  game.roll(5)
+end
+
+def roll_strike
+  game.roll(10)
+end
+
 describe Game do
+  let(:game) { Game.new }
+
   describe "#roll" do
     it "responds to #roll" do
-      game = Game.new
       expect(game).to respond_to(:roll)
     end
   end
 
   describe "#score" do
     it "responds to #roll" do
-      game = Game.new
       expect(game).to respond_to(:score)
+    end
+
+    it "Gutter game scores zero - When you roll all misses, you get a total score of zero." do
+      roll_many(0, 20)
+      expect(game.score).to eq(0)
+    end
+
+    it "All ones scores 20 - When you knock down one pin with each ball, your total score is 20." do
+      roll_many(1, 20)
+      expect(game.score).to eq(20)
+    end
+
+    it "A spare in the first frame, followed by three pins, followed by all misses scores 16." do
+      roll_spare
+
+      game.roll(3)
+
+      roll_many(0, 17)
+      expect(game.score).to eq(16)
+    end
+
+    it "A strike in the first frame, followed by three and then four pins, followed by all misses, scores 24." do
+      roll_strike
+      game.roll(3)
+
+      game.roll(4)
+
+      roll_many(0, 16)
+      expect(game.score).to eq(24)
+    end
+
+    it "A perfect game (12 strikes) scores 300" do
+      roll_many(10, 12)
+      expect(game.score).to eq(300)
     end
   end
 end
